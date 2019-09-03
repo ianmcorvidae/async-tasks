4b5a325c3225f8a3fa7f57f04921ad7b08382fdc
Ian McEwen <mian@cyverse.org>
2019-09-03 16:47:58 -0700

    add proper 404 when task doesn't exist, start on filtered-list endpoint

--- app.go
+++ app.go
@@ -24,7 +24,8 @@ func NewAsyncTasksApp(db *DBConnection, router *mux.Router) *AsyncTasksApp {
 }
 
 func (a *AsyncTasksApp) InitRoutes() {
-	a.router.HandleFunc("/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}", app.GetByIdRequest).Methods("GET").Name("getById")
+	a.router.HandleFunc("/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}", a.GetByIdRequest).Methods("GET").Name("getById")
+	// a.router.HandleFunc("/", a.GetByFilterRequest).Methods("GET").Name("getByFilter")
 	// list multiple tasks by filter
 	// post new task
 	// delete by ID
@@ -48,12 +49,19 @@ func (a *AsyncTasksApp) GetByIdRequest(writer http.ResponseWriter, r *http.Reque
 	tx, err := a.db.BeginTx(context.TODO(), nil)
 	if err != nil {
 		errored(writer, err.Error())
+		return
 	}
 	defer tx.tx.Rollback()
 
 	task, err := tx.GetTask(id)
 	if err != nil {
 		errored(writer, err.Error())
+		return
+	}
+
+	if task.ID == "" {
+		notFound(writer, "not found")
+		return
 	}
 
 	log.Info(task)
@@ -61,12 +69,26 @@ func (a *AsyncTasksApp) GetByIdRequest(writer http.ResponseWriter, r *http.Reque
 	jsoned, err := json.Marshal(task)
 	if err != nil {
 		errored(writer, err.Error())
+		return
 	}
 
 	writer.Write(jsoned)
 
 	return
 }
+
+//func (a *AsyncTasksApp) GetByFilterRequest(writer http.ResponseWriter, r *http.Request) {
+//	var (
+//		v = r.URL.Query()
+//
+//		types       string
+//		statuses    string
+//		usernames   string
+//		start_dates string
+//		end_dates   string
+//	)
+//
+//}
 
 func badRequest(writer http.ResponseWriter, msg string) {
         http.Error(writer, msg, http.StatusBadRequest)
@@ -78,3 +100,7 @@ func errored(writer http.ResponseWriter, msg string) {
         log.Error(msg)
 }
 
+func notFound(writer http.ResponseWriter, msg string) {
+        http.Error(writer, msg, http.StatusNotFound)
+        log.Error(msg)
+}
